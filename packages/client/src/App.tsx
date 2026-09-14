63d210cf8893a13dcefdc745adab43803b7b4cae
Seran R <imhariseran@gmail.com>
2026-09-14 18:48:09 +0530

    Set up Shadcn/ui

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,4 +1,5 @@
 import {useEffect, useState} from 'react';
+import { Button } from './components/ui/button';
 
 function App() {
   const [message, setMessage] = useState('');
@@ -9,7 +10,13 @@ function App() {
       .catch((error) => console.error('Error fetching message:', error));
   }, []);
 
-  return <p className='font-bold p-4 text-3xl'>{message}</p>;
+  return (
+    <div className='p-4'>
+      <p className='font-bold text-3xl'>{message}</p>
+      <Button>Click me</Button>
+    </div>
+  
+);
 }
 
 export default App
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
@@ -6,6 +7,11 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   cacheDir: '../../node_modules/.vite/client',
   plugins: [react(), tailwindcss()  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
