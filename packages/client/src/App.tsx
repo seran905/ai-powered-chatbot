98845c119139096dca65614d641d8583a70f0ebe
Seran R <imhariseran@gmail.com>
2026-09-15 00:24:21 +0530

    Build ChatBot component

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,20 +1,9 @@
-import { useEffect, useState } from 'react';
-import { Button } from './components/ui/button';
+import ChatBot from './components/ChatBot';
 
 function App() {
-   const [message, setMessage] = useState('');
-
-   useEffect(() => {
-      fetch('/api/hello')
-         .then((response) => response.json())
-         .then((data) => setMessage(data.message))
-         .catch((error) => console.error('Error fetching message:', error));
-   }, []);
-
    return (
       <div className="p-4">
-         <p className="font-bold text-3xl">{message}</p>
-         <Button>Click me!</Button>
+         <ChatBot />
       </div>
    );
 }
