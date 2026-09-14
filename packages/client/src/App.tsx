8188b52b4296482323df5ee14bb644342649ecda
Seran R <imhariseran@gmail.com>
2026-09-14 18:16:22 +0530

    Set up Tailwindcss

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       .catch((error) => console.error('Error fetching message:', error));
   }, []);
 
-  return <p>{message}</p>;
+  return <p className='font-bold p-4 text-3xl'>{message}</p>;
 }
 
 export default App
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   cacheDir: '../../node_modules/.vite/client',
-  plugins: [react()],
+  plugins: [react(), tailwindcss()  ],
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
