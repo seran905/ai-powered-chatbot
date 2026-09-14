6e5d91c6edabd2f065c1072888a9306f6b6b7665
Seran R <imhariseran@gmail.com>
2026-09-14 19:19:22 +0530

    Set up Prettier and Format code with it

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,22 +1,22 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState('');
-  useEffect(() => {
-    fetch('/api/hello')
-      .then((response) => response.json())
-      .then((data) => setMessage(data.message))
-      .catch((error) => console.error('Error fetching message:', error));
-  }, []);
+   const [message, setMessage] = useState('');
 
-  return (
-    <div className='p-4'>
-      <p className='font-bold text-3xl'>{message}</p>
-      <Button>Click me</Button>
-    </div>
-  
-);
+   useEffect(() => {
+      fetch('/api/hello')
+         .then((response) => response.json())
+         .then((data) => setMessage(data.message))
+         .catch((error) => console.error('Error fetching message:', error));
+   }, []);
+
+   return (
+      <div className="p-4">
+         <p className="font-bold text-3xl">{message}</p>
+         <Button>Click me</Button>
+      </div>
+   );
 }
 
-export default App
+export default App;
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,20 +1,20 @@
-import path from 'path'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  cacheDir: '../../node_modules/.vite/client',
-  plugins: [react(), tailwindcss()  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3000',
-    }
-  }
-})
+   cacheDir: '../../node_modules/.vite/client',
+   plugins: [react(), tailwindcss()],
+   resolve: {
+      alias: {
+         '@': path.resolve(__dirname, './src'),
+      },
+   },
+   server: {
+      proxy: {
+         '/api': 'http://localhost:3000',
+      },
+   },
+});
