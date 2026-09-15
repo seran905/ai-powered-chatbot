a24fa001a24506bccd0fee5a85abea800061de93
Seran R <imhariseran@gmail.com>
2026-09-15 22:28:42 +0530

    Render markdown text with react-markdown

--- packages/client/src/components/ChatBot.tsx
+++ packages/client/src/components/ChatBot.tsx
@@ -1,5 +1,6 @@
-import { useRef, useState, type KeyboardEvent } from 'react';
 import axios from 'axios';
+import { useRef, useState, type KeyboardEvent } from 'react';
+import ReactMarkdown from 'react-markdown';
 import { useForm } from 'react-hook-form';
 import { FaArrowUp } from 'react-icons/fa';
 import { Button } from './ui/button';
@@ -52,7 +53,7 @@ const ChatBot = () => {
                         : 'bg-gray-100 text-black self-start'
                   }`}
                >
-                  {message.content}
+                  <ReactMarkdown>{message.content}</ReactMarkdown>
                </p>
             ))}
          </div>
