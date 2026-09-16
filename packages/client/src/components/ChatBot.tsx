ac84e854aa993befd385cf6f4a12bb32afafa7dd
Seran R <imhariseran@gmail.com>
2026-09-16 22:32:40 +0530

    improving the look and feel

--- packages/client/src/components/ChatBot.tsx
+++ packages/client/src/components/ChatBot.tsx
@@ -21,18 +21,18 @@ type Message = {
 const ChatBot = () => {
    const [messages, setMessages] = useState<Message[]>([]);
    const [isBotTyping, setIsBotTyping] = useState(false); // State to track if the bot is typing
-   const formRef = useRef<HTMLFormElement | null>(null);
+   const lastMessageRef = useRef<HTMLDivElement | null>(null);
    const conversationId = useRef(crypto.randomUUID());
    const { register, handleSubmit, reset, formState } = useForm<FormData>();
 
    useEffect(() => {
-      formRef.current?.scrollIntoView({ behavior: 'smooth' });
+      lastMessageRef.current?.scrollIntoView({ behavior: 'smooth' });
    }, [messages]);
 
    const onSubmit = async ({ prompt }: FormData) => {
       setMessages((prev) => [...prev, { content: prompt, role: 'user' }]);
 
-      reset(); // Reset the form after submission
+      reset({ prompt: '' }); // Reset the form after submission
       setIsBotTyping(true); // Set the bot as typing
       const { data } = await axios.post<ChatResponse>('api/chat', {
          prompt,
@@ -58,12 +58,13 @@ const ChatBot = () => {
    };
 
    return (
-      <div>
-         <div className="flex flex-col gap-3 mb-10">
+      <div className="flex flex-col h-full">
+         <div className="flex flex-col flex-1 gap-3 mb-10 overflow-y-auto">
             {messages.map((message, index) => (
-               <p
+               <div
                   key={index}
                   onCopy={onCopyMessage}
+                  ref={index === messages.length - 1 ? lastMessageRef : null}
                   className={`px-3 py-1 rounded-xl ${
                      message.role === 'user'
                         ? 'bg-blue-600 text-white self-end'
@@ -71,7 +72,7 @@ const ChatBot = () => {
                   }`}
                >
                   <ReactMarkdown>{message.content}</ReactMarkdown>
-               </p>
+               </div>
             ))}
             {isBotTyping && (
                <div className="flex gap-1 px-3 py-3 bg-gray-200 rounded-xl self-start">
@@ -82,7 +83,6 @@ const ChatBot = () => {
             )}
          </div>
          <form
-            ref={formRef}
             onSubmit={handleSubmit(onSubmit)}
             onKeyDown={onKeyDown}
             className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
@@ -92,6 +92,7 @@ const ChatBot = () => {
                   required: true,
                   validate: (value) => value.trim().length > 0,
                })}
+               autoFocus
                className="w-full border-0 focus:outline-0 resize-none"
                placeholder="Ask me anything..."
                maxLength={1000}
